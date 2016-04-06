e1e149e2ea8900d31f291fa0e3c544d234800903
jayce wang <jayce wang>
2016-04-06 18:57:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mychess/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.administrator.mychess;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    GameView gameView = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 全屏显示
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // 获取屏幕宽高
+        Display display = getWindowManager().getDefaultDisplay();
+        // 现实GameView
+        GameView.init(this, display.getWidth(), display.getHeight());
+        gameView = GameView.getInstance();
+        setContentView(gameView);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return super.onKeyDown(keyCode, event);
+    }
+}
